d554a44316bf18c904735a40784c8e30640251b9
Bhaskar KC <bhaskarkc2000@gmail.com>
2026-01-22 07:09:02 +0545

    Snake game feature added

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# """
+# git status --> hami kun branch ma chham, k k vayo file change
+# git add . --> This is used to add the changes to the staging area. This tells git hub, which files has been changes we want to add to the next commit.
+# git commit -m "commit message" --> k kam gareko message, this makes a snapshot of the changes we have made.
+# git push origin main --> remote repository ma push garne, origin --> name of the remote repository, 
+# main(default branch) --> name of the branch we want to push to.
+# git commit 
+# """
+
+# staging area -> in next time commit, which changes to include done here. 
+
